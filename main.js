2c8a2c78624e5e075f142cdb1e76124981b9a863
sneha-860 <snehakalra74@gmail.com>
2024-10-19 19:10:45 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed=new Typed(".text",{
+    strings:["Web Developer","Machine Learning Enthusiast","Problem Solver"] ,
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+});
